418ca49d16bd0706cd6c08610b1ed875527a2523
hubol01 <kuloleg52@gmail.com>
2024-03-25 18:45:01 +0300

    fixed listWidget

--- src/containers/ListWidget.tsx
+++ src/containers/ListWidget.tsx
@@ -1,39 +1,31 @@
-import React from 'react'
-import { useQuery } from 'react-query';
-import { ScheduleService } from '../core/services/ScheduleAPI.service';
 import CardLesson from '../components/ui/CardLesson';
 import { ILesson, ISchedule } from '../models/scheduleModel';
 
-interface ListWidgetProps{
-    ifacult: number,
-    igroup: number,
-    week: number,
-    weekday: number,
-    data: ISchedule
+interface ListWidgetProps {
+  ifacult: number,
+  igroup: number,
+  week: number,
+  weekday: number,
+  data: ISchedule
 }
 
-export default function ListWidget({ifacult, igroup, week, weekday, data}: ListWidgetProps) {
-  //   const { data } = useQuery(['schedules'], () =>
-  //   ScheduleService.getAll()
-  // )
-    function listLessons(week: number, weekday: number): ILesson[] {
-        // const max: number = data!.faculties[ifacult].groups[igroup].lessons.length;
-        const list: ILesson[] = [];
-        for (const i in data.faculties[ifacult].groups[igroup].lessons) {
-          if (data!.faculties[ifacult].groups[igroup].lessons[i].date.week === week &&
-              data!.faculties[ifacult].groups[igroup].lessons[i].date.weekday === weekday) {
-            list.push(data!.faculties[ifacult].groups[igroup].lessons[i]);
-          }
-        }
-        return list;
+export default function ListWidget({ ifacult, igroup, week, weekday, data }: ListWidgetProps) {
+  function listLessons(week: number, weekday: number): ILesson[] {
+    const list: ILesson[] = [];
+    for (const i in data.faculties[ifacult].groups[igroup].lessons) {
+      if (data!.faculties[ifacult].groups[igroup].lessons[i].date.week === week &&
+        data!.faculties[ifacult].groups[igroup].lessons[i].date.weekday === weekday) {
+        list.push(data!.faculties[ifacult].groups[igroup].lessons[i]);
       }
-      
+    }
+    return list;
+  }
+
   return (
     <>
-    {/* {data ? data.faculties.length : <></>} */}
-    {data ?/* ? listLessons(week, weekday).map((lesson)=>{ */
-        <CardLesson lesson={data!.faculties[ifacult].groups[igroup].lessons[0]}/>
-    /*  }) */: <></>}
+      {data ? listLessons(week, weekday).map((lesson) => (
+        <CardLesson lesson={lesson} />
+      )) : <>Нет данных</>}
     </>
   )
 }
